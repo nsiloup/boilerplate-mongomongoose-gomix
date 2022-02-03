08770368c99d7f0f8582acfe62fd3ce43e340a32
NSILOU BALANDAMIO perseverance <nsiloup@gmail.com>
2022-02-04 00:22:33 +0100

    Create a Model --->>> CRUD Part I - CREATE

--- myApp.js
+++ myApp.js
@@ -2,7 +2,15 @@ require('dotenv').config();
 let mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-let Person;
+
+//Create a Model --->>> CRUD Part I - CREATE
+let { Schema } = mongoose;
+let personSchema = new Schema({
+  name: String,
+  age: Number,
+  favoriteFoods : Array
+})
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
